4e92659e495a66beee8e97e34c06b6b634b7e3c6
Alex <ami80@uclive.ac.nz>
2018-03-23 20:47:19 +1300

    Update reqs, update to reconnect on connection error after 60 seconds

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 from discordant import Discordant, configure_logging
+from time import sleep
 
 
 if __name__ == '__main__':
     configure_logging()
+    exit = False
 
     bot = Discordant()
-    try:
-        bot.run()
-    except KeyboardInterrupt:
-        print('Exiting...')
+
+    while not exit:
+        try:
+            bot.run()
+        except ConnectionResetError as error:
+            print(error)
+            print('Reconnecting in 60 seconds...')
+            sleep(60)
+        except KeyboardInterrupt:
+            exit = True
+            print('Exiting...')
